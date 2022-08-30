0ae4a7ba49d4564bdac13d521e174840073e0a45
kkohler2 <kenkohler@hotmail.com>
2022-08-30 16:00:34 -0500

    FileObserver example

---
+++ FileObserver/FileObserver.Android/Services/FileObserverService.cs
@@ -0,0 +1,25 @@
+﻿using FileObserver.Droid.Services;
+using System;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(FileObserverService))]
+namespace FileObserver.Droid.Services
+{
+    public class FileObserverService : IFileObserverService
+    {
+        private TestFileObserver _testFileObserver;
+        private const string _filePath = "/sdcard";
+
+        [Obsolete]
+        public FileObserverService()
+        {
+            _testFileObserver = new TestFileObserver(_filePath);
+            _testFileObserver.ScanFile += ReadHIDScanFile;
+        }
+
+        private void ReadHIDScanFile(object sender, string file)
+        {
+            // Do something here with observed file...
+        }
+    }
+}
---
+++ FileObserver/FileObserver.Android/Services/TestFileObserver.cs
@@ -0,0 +1,27 @@
+﻿using Android.OS;
+using Android.Util;
+using System;
+
+namespace FileObserver.Droid.Services
+{
+    public class TestFileObserver : Android.OS.FileObserver
+    {
+        static FileObserverEvents _Events = (FileObserverEvents.CloseWrite);
+        public event EventHandler<string> ScanFile;
+
+        [Obsolete]
+        public TestFileObserver(string rootPath) : base(rootPath, _Events)
+        {
+            Log.Debug("FileObserver",$"Watching folder for HID badge swipes: {rootPath}");
+            StartWatching();
+        }
+
+        public override void OnEvent(FileObserverEvents e, string path)
+        {
+            //if (path.EndsWith(".txt"))
+            {
+                ScanFile?.Invoke(this, path);
+            }
+        }
+    }
+}
